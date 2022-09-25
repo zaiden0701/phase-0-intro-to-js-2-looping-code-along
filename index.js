4fc49fa795afcbc27d99f4064a5ad5986af67c96
Aiden Zafar <+>
2022-09-25 18:48:46 -0400

    commit tree d17002750de582b9ea9bf32d3a1a6dbac28d7e4d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name,event){
+    const messages = []
+    for (let i = 0; i < name.length; i++){
+        const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+        messages.push(message)
+    }
+    return messages
+}
+
+function countDown(int){
+    let countdown = int
+    while (countdown >=0)
+        console.log(countdown--)
+    }
+
+countDown(int)
